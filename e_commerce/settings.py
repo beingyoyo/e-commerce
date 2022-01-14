cffa38a97e1aa8716848951098ffc7b5bbb5afac
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-14 20:20:25 +0530

    added category app

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'e_commerce',
+    'category',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
