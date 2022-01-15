410826d1325d32e7073441e74f0cf643b59fadb8
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-15 14:49:58 +0530

    added product class

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
+from unicodedata import category
 from django.db import models
-
+from category.models import Category
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    product_description = models.TextField(max_length=200, blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to = 'photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.product_name
