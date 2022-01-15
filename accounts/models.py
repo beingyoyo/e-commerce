e1ec2ffebf4e50a3d4d29c58efff751c4ca042da
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-15 12:12:57 +0530

    added the custom model for users

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,35 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class MyAccountManager(AbstractBaseUser):
+    def create_user(self, first_name, last_name, username, email, password = None):
+        if not email:
+            raise ValueError('User must have an email')
+
+        if not username:
+            raise ValueError('User must have an username')
+        
+        user = self.model(
+            email = self.normalize_email(email), #normalize the e-mail, if caps entered, will be noramlized to smallcase
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(email=self.normalize_email(email), username=username, password=password,first_name=first_name, last_name=last_name)
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True 
+        user.save(using=self._db)
+        return user
+
+
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -26,6 +55,8 @@ class Account(AbstractBaseUser):
     #Login with email setting
     USERNAME_FIELD = 'email'
     REQUIRED_FIELD = ['username', 'firstname', 'lastname']
+
+    objects = MyAccountManager()
 
     def __str__(self) -> str:
         return self.email
