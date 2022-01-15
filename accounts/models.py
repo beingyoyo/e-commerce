7a7d1b99d6d79569fb81dc7bb4838205256bd65f
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-15 11:59:48 +0530

    added account class

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,37 @@
+import email
+import modulefinder
+from operator import mod
+from statistics import mode
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    #required
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+
+    #checking the status of the user
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    #Login with email setting
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['username', 'firstname', 'lastname']
+
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm, obj = None):
+        return self.is_admin
+
+    def has_module_perm(self, add_label):
+        return True
