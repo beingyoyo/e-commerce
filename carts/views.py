aa8c00b4d39849ffb1d1d76ecd2d813471905717
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-21 20:30:48 +0530

    variations added to cart

--- carts/views.py
+++ carts/views.py
@@ -55,6 +55,9 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variations.add(item) 
         cart_item.quantity += 1 #increase cart items value
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -63,6 +66,9 @@ def add_cart(request, product_id):
             quantity = 1,
             cart = cart,
         )
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variations.add(item)
         cart_item.save()
     return redirect('cart')
 
