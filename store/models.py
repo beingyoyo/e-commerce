31d1340886b69df4ffbb670175f6f6298dd6d36d
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-21 19:46:09 +0530

    variation object changes

--- store/models.py
+++ store/models.py
@@ -20,6 +20,14 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.product_name
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def size(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)  
+
 variation_category_choice = (
     ('color', 'color'),
     ('size', 'size'),
@@ -29,7 +37,9 @@ class Variation(models.Model):
     variation_category = models.CharField(max_length=100, choices=variation_category_choice)
     variation_value =models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
-    created_date = models.DateField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now=True)
 
-    def __unicode__(self) -> str:
-        return self.product
+    objects = VariationManager()
+
+    def __str__(self) -> str:
+        return self.variation_value
