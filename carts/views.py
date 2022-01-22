44b0b07a3d1318751328252f7db8c0289aa935e0
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-22 12:27:59 +0530

    changes to the variation count

--- carts/views.py
+++ carts/views.py
@@ -53,22 +53,43 @@ def add_cart(request, product_id):
         )
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        if len(product_variation) > 0:
-            for item in product_variation:
-                cart_item.variations.add(item) 
-        cart_item.quantity += 1 #increase cart items value
-        cart_item.save()
-    except CartItem.DoesNotExist:
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        #existing variations -> database
+        #current variations -> product_variation
+        #item id -> database
+        ex_var_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+        print(existing_variation)
+
+        if product_variation in ex_var_list:
+            #increase cart item
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+        else:
+            #create cart item
+            item = CartItem.objects.create(product = product, quantity = 1, cart = cart)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation) # * adds all product variations
+            cart_item.save()
+    else:
         cart_item = CartItem.objects.create(
             product = product,
             quantity = 1,
             cart = cart,
         )
         if len(product_variation) > 0:
-            for item in product_variation:
-                cart_item.variations.add(item)
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
         cart_item.save()
     return redirect('cart')
 
