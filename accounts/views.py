9a93a54c303c57bcd3706deb16d493b9a0d9be38
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-23 17:28:23 +0530

    accounts urls and views initiated

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    return render(request, template_name='accounts/register.html')
+
+def login(request):
+    return render(request, template_name='accounts/login.html')
+
+def logout(request):
+    return 
