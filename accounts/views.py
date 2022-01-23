4123487260a5615f79a09cc0e977c7e01d345b28
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-23 19:32:04 +0530

    getting form data into views

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
+from accounts.models import Account
 from .forms import RegistrationForm
 
 # Create your views here.
 def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = Account.objects.create_user()
     form = RegistrationForm()
     context = {
         'form' : form
