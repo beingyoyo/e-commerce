ddd26813f218e165c30ee62fcc4e574ebc2544bd
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-27 12:25:21 +0530

    verfication changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from base64 import urlsafe_b64encode
 from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.shortcuts import render, redirect
@@ -38,7 +38,7 @@ def register(request):
             message = render_to_string('accounts/account_verification_email.html', {
                 'user':user,
                 'domain' : current_site,
-                'uid' : urlsafe_base64_decode(force_bytes(user.pk)),
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
                 'token' : default_token_generator.make_token(user)
             })
 
@@ -46,8 +46,8 @@ def register(request):
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send() 
             
-            messages.success(request, 'Registration Successful!')
-            return redirect('register')
+            #messages.success(request, 'Thank you for registering with us. We have mailed you an activation link to join our family :)')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()  
     
@@ -76,3 +76,19 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out!')
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congrats! You are a member of "Ohh & Eff" Family')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link!')
+        return redirect('register') 
--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -142,7 +142,7 @@ MESSAGE_TAGS = {
 #SMTP CONFIG
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yash.makwana@somaiya.edu'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 # Default primary key field type
