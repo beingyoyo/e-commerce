5df08a4eba4d629b098c18657a004091450da599
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-29 11:26:54 +0530

    changes

--- carts/views.py
+++ carts/views.py
@@ -113,3 +113,6 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
