de593b155ee7a92def6ad479c9789aaa8c4fcfa8
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-01-29 13:24:30 +0530

    checkout changes

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from carts.models import Cart, CartItem
 from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def cart(request, total = 0, quantity = 0, cart_items = None):
@@ -114,5 +115,24 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item.delete()
     return redirect('cart')
 
-def checkout(request):
-    return render(request, 'store/checkout.html')
+#@login_required(login_url='login')
+def checkout(request, total = 0, quantity = 0, cart_items = None):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (5 *  total)/100
+        grand_total = tax + total
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items,
+        'grand_total' : grand_total,
+        'tax' : tax
+    }
+    return render(request, 'store/checkout.html', context=context)
