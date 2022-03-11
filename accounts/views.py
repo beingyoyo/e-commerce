c0487880a33a55b44978855c7aab95b16093f407
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-03-11 17:24:45 +0530

    product gallery model added

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ from carts.models import Cart, CartItem
 from carts.views import _cart_id
 from .forms import RegistrationForm, UserForm, UserProfileForm
 import requests
-from orders.models import Order
+from orders.models import Order, OrderProduct
 
 # Create your views here.
 def register(request):
@@ -130,8 +130,10 @@ def logout(request):
 def dashboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered = True)
     orders_count = orders.count()
+    userprofile = UserProfile.objects.get(user_id = request.user.id)
     context = {
-        'orders_count' : orders_count
+        'orders_count' : orders_count,
+        'userprofile' : userprofile
     }
     return render(request, template_name='accounts/dashboard.html', context=context)
 
@@ -266,3 +268,16 @@ def change_password(request):
             messages.error(request, 'Password does not match')
             return redirect('change_password')
     return render(request, 'accounts/change_password.html')
+
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number = order_id)
+    order = Order.objects.get(order_number = order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context = {
+        'order_detail' : order_detail,
+        'order' : order,
+        'subtotal' : subtotal
+    }
+    return render(request, 'accounts/order_detail.html', context=context)
